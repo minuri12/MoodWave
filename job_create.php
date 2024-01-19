e5117c0a799dcb5c3e8d6fd88adedd230b2eacb6
kusha2000 <kushanandarawewa1@gmail.com>
2024-01-19 22:42:15 +0530

    Jobs Page Created

--- job_create.php
+++ job_create.php
@@ -17,6 +17,9 @@ if(isset($message)){
 
 session_start();
 
+//testing
+$user_id = 1;
+
 //=====We have to give this in the final========
 
 // $user_id = $_SESSION['user_id'];
@@ -94,6 +97,45 @@ if(isset($_POST['post_job'])){
         
     </form>
 
+</section>
+<section class="my-jobs">
+    <h1>My Jobs</h1>
+    <table>
+        <tr>
+            <th>Job Name</th>
+            <th>Needed Listners</th>
+            <th>Completed</th>
+        </tr>
+
+            <?php
+            
+            $select_jobs = mysqli_query($conn, "SELECT * FROM `jobs` WHERE creater_id='$user_id'") or die('query failed');
+            if(mysqli_num_rows($select_jobs) > 0){
+                while($fetch_jobs = mysqli_fetch_assoc($select_jobs)){
+            ?>
+            <tr>
+                <td> <?php echo $fetch_jobs['song_name']; ?></td>
+                <td> <?php echo $fetch_jobs['workers_need']; ?></td>
+                
+                <td> <?php 
+                $complete_no=$fetch_jobs['completed']; 
+                $works=$fetch_jobs['workers_need'];
+                $persontage=$complete_no/$works *100;
+
+                if($persontage>75){
+                    echo  '<div class="cbox" style="display:flex;">'.intval($persontage).'% <div style="background-color:green;width:30px;height:10px;border-radius: 3px;margin-top:8px;margin-left:10px"></div></div>';
+                }elseif($persontage>45){
+                    echo  '<div class="cbox" style="display:flex;">'.intval($persontage).'% <div style="background-color:yellow;width:30px;height:10px;border-radius: 3px;margin-top:8px;margin-left:10px"></div></div>';
+                }else{
+                    echo  '<div class="cbox" style="display:flex;">'.intval($persontage).'% <div style="background-color:red;width:30px;height:10px;border-radius: 3px;margin-top:8px;margin-left:10px"></div></div>';
+                }
+                ?></td>
+            </tr>
+            <?php
+                }
+            }
+            ?>
+    </table>
 </section>
     <script src="https://kit.fontawesome.com/f05855486d.js" crossorigin="anonymous"></script>
     <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> -->
