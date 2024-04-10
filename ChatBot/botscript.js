ff28dabcefaa939b637450add45cd40648f9bfa5
JayaruPerera <jayaruperera1998@gmail.com>
2024-04-10 13:08:57 +0530

    Adding the entered message in the chatbox

---
+++ ChatBot/botscript.js
@@ -0,0 +1,24 @@
+const chatInput = document.querySelector('.chat-input textarea');
+const sendChatBtn = document.querySelector('.chat-input span');
+
+
+let userMessage;
+
+const createChatLi = (message, className) => {
+    const chatLi = document.createElement ('li');
+    chatLi.classList.add('chat', className);
+    let chatContent = className === 'outgoing' ? `<p></p>` : `<span class='material-symbols-outlined'>smart_toy</span><p></p>`;
+    chatLi.innerHTML = chatContent;
+    chatLi.querySelector('p').textContent = message;
+    return chatLi;
+}
+
+const handleChat = () => {
+    userMessage = chatInput.value.trim();
+    if(!userMessage) return;
+
+    chatbox.appendChild(createChatLi(userMessage, 'outgoing'));
+
+}
+
+sendChatBtn.addEventListener('click',handleChat);
